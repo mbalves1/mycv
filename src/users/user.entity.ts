be82a1820a35c6fdec020a17221380379b8951c3
Murilo <mbalves1@outlook.com>
2025-01-07 14:05:07 -0300

    `[feat]: Added class-validator and class-transformer dependencies, updated package-lock.json and package.json, and implemented validation using ValidationPipe in main.ts`

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -13,4 +20,19 @@ export class User {
 
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Insert User with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Update User with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Remove User with id', this.id);
+  }
 }
