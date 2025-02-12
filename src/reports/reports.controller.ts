12fd2fe8a02b2f1de6845a570ec711f42f4dcbef
Murilo <mbalves1@outlook.com>
2025-02-12 15:50:49 -0300

    `[feat]: Added approval feature to reports: added approved field to Report entity and Report DTO, created ApproveReportDto, updated ReportsController to handle approval, and updated ReportsService to change report approval status.`

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,11 +1,19 @@
 import { ReportsService } from './reports.service';
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { User } from 'src/users/user.entity';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
+import { ApproveReportDto } from './dtos/approve-report.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -16,5 +24,10 @@ export class ReportsController {
   @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportsService.createReport(body, user);
+  }
+
+  @Patch('/:id')
+  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    return this.reportsService.changeApproval(id, body.approved);
   }
 }
