23530f108f5c3cc99319b3a4c96e5a8a6fce5b33
Murilo <mbalves1@outlook.com>
2025-02-17 10:30:45 -0300

    `[feat]: Added new endpoint for getting estimate and updated request.http file to include new endpoint`

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -6,6 +6,8 @@ import {
   Patch,
   Post,
   UseGuards,
+  Get,
+  Query,
 } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { AuthGuard } from 'src/guards/auth.guard';
@@ -15,10 +17,16 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { AdminGuard } from 'src/guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    console.log(query);
+  }
 
   @Post()
   @UseGuards(AuthGuard)
